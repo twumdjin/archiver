021f0c3e2b79d806e59b3ccd5a64dd7701ea95e0
Twum Djin <twumdjin@gmail.com>
2014-08-10 22:07:21 -0700

    Add more countries, lists, crontab to run daily

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer',  platforms: :ruby
+gem 'therubyracer',  platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -40,6 +40,10 @@ gem 'spring',        group: :development
 
 # Nokogiri 
 gem 'nokogiri'
+gem 'faraday'
+gem 'faraday_middleware'
+gem 'whenever', :require => false
+
 group :test do
   gem 'rspec', :require => 'spec'
 end
--- bin/appstore/download_toplist.rb
+++ bin/appstore/download_toplist.rb
@@ -1,10 +1,93 @@
 #!/usr/bin/env ruby
 
-require 'nokogiri'
+require 'faraday'
+require 'faraday_middleware'
+require 'fileutils'
+require 'json'
+require 'open-uri'
+require 'zlib'
+
+# Define download class
+# Probably should live elsewhere...
+class HttpClient
+  def get(url)
+    # Default to something bad
+    code = 0
+    begin
+      conn = ::Faraday.new(url) do |c|
+        c.use ::FaradayMiddleware::FollowRedirects
+        c.adapter ::Faraday.default_adapter
+      end
+      conn.headers = {
+        'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language' => 'en-US,en;q=0.5',
+        'Accept-Encoding' => 'gzip, deflate',
+        'Connection' => 'keep-alive',
+      }
+      response = conn.get
+      if response.respond_to?(:status)
+        code = response.status.to_i
+      end
+    rescue OpenURI::HTTPRedirect => e
+      return nil, 302, response
+    rescue EOFError, OpenURI::HTTPError, RuntimeError => e
+      puts("Failed to execute GET request to #{url}. Error was #{e.class} #{e.message}")
+      code = 0
+    end
+    if code < 200 || code > 300
+      puts "response code was #{code}"
+      puts "response was #{response.body}" if response
+      data = nil
+    elsif response.headers['Content-Encoding'] == 'gzip' then
+      begin
+        data = Zlib::GzipReader.new(StringIO.new(response.body)).read
+      rescue Zlib::DataError => e
+        data = nil
+        puts("Error unzipping data from #{url}. Error was #{e.class} #{e.message}")
+      end
+    else
+      data = response.body
+    end
+    data
+  end
+  
+  def save_remote(url, file_name)
+    dirname = File.dirname(file_name)
+    FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
+    if File.exist?(file_name)
+      puts "Skipping existing file: #{file_name}"
+      return
+    end
+    data = self.get(url)
+    raise "Failed to save resource at #{url} to file #{file_name}" unless data
+    File.open(file_name, 'w') do |f|
+      f.write(data)
+    end
+    puts "Successfully saved file: #{file_name}"
+  end
+end
 
 # Enumerate endpoints for rss
-TOPLISTS = %w()
+DEFAULT_LIMIT = 200
+COUNTRIES = %w(us gb au ca hk kr cn jp in fr)
+TOPLISTS = %w(topfreeapplications toppaidapplications topgrossingapplications topfreeipadapplications toppaidipadapplications topgrossingipadapplications newapplications newfreeapplications newpaidapplications)
+URL_FORMAT = 'https://itunes.apple.com/%{country_code}/rss/%{list_name}/limit=%{limit}/json'
+FILE_FORMAT = '/import/appstore/toplist/%{list_name}/%{date}/%{country_code}.json'
 
+# Format date string
+date = Time.now.strftime('%Y/%m/%d')
 # Iterate over endpoints and fetch rss for today
+http_client = HttpClient.new
+write_count = 0
+COUNTRIES.each do |country_code|
+  TOPLISTS.each do |list_name|
+    params = {country_code: country_code, list_name: list_name, date: date, limit: DEFAULT_LIMIT}
+    url = URL_FORMAT % params
+    file_name = FILE_FORMAT % params
+    http_client.save_remote(url, file_name)
+    write_count += 1
+  end
+end
 
-puts 'hello world'
+puts "Written #{write_count} files today"
